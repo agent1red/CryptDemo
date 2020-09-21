c565f4c6c21eeadfa042fcd2a2f9aaf2f2a9193a
Kevin Hudson <darklen14@gmail.com>
2020-09-21 10:21:40 -0400

    FIrst Commit

---
+++ Source/CryptDemo/CryptDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CryptDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CryptDemo, "CryptDemo" );
---
+++ Source/CryptDemo/CryptDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CryptDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRYPTDEMO_API ACryptDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
